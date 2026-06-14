23dbd89e5ca5cfc89f7ec6361e023a3629be7dc2
vikar <vikarudenko51@gmail.com>
2026-06-14 22:40:16 +0200

    add average sales comment

--- ANALYSIS.py
+++ ANALYSIS.py
@@ -12,3 +12,9 @@ data = {"city": ["Kyiv", "Lviv", "Odesa"], "sales": [1200, 950, 500]}
 
 
 print("Продажі по містах (тимчасова версія):")
+
+
+
+average_sales = df["sales"].mean()
+print("Середнє значення:", average_sales)
+print("Це середній рівень продажів по трьох містах")
